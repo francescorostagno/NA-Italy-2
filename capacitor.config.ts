eff82466b09ac690f67a5da790855045e1e978ed
Paul Nagle <paulnagle@gmail.com>
2024-06-08 14:14:03 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.naitalia.app',
+  appName: 'Na-Italy-3',
+  webDir: 'www'
+};
+
+export default config;
